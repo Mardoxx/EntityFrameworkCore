1b707f05f7e927b12e682de0669b74774be31d90
Smit Patel <smitpatel@users.noreply.github.com>
2017-11-08 15:02:05 -0800

    Design: Put system namespaces first in generated code Don't generate empty namespace if context is not in namespce
    Resolves #10225
    Resolves #2467

---
+++ src/EFCore.Design/Design/Internal/NamespaceComparer.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.EntityFrameworkCore.Design.Internal
+{
+    /// <summary>
+    ///     A custom string comparer to sort using statements to have System prefixed namespaces first.
+    /// </summary>
+    public class NamespaceComparer : IComparer<string>
+    {
+        /// <inheritdoc />
+        public virtual int Compare(string x, string y)
+            => x?.StartsWith("System", StringComparison.Ordinal) == true
+                ? (y?.StartsWith("System", StringComparison.Ordinal) == true
+                    ? string.CompareOrdinal(x, y)
+                    : -1)
+                : (y?.StartsWith("System", StringComparison.Ordinal) == true
+                    ? 1
+                    : string.CompareOrdinal(x, y));
+    }
+}
